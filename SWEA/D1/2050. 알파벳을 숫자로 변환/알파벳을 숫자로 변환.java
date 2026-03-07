b20599dacfaf086e8fde2cde399929993a824a44
Mukjin <127205198+Mukjin@users.noreply.github.com>
2026-03-07 23:30:13 +0900

    [D1] Title: 알파벳을 숫자로 변환, Time: 90 ms, Memory: 26,880 KB -BaekjoonHub

---
+++ SWEA/D1/2050. 알파벳을 숫자로 변환/알파벳을 숫자로 변환.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		for (int i = 0; i < s.length(); i++) {
+			System.out.print(((int)s.charAt(i)-64)+" ");
+		}
+		sc.close();
+	}
+	
+	
+}
