eecfa2e408b696068d047d7ca175a14d1b5898dd
Mukjin <127205198+Mukjin@users.noreply.github.com>
2026-03-13 00:52:56 +0900

    [D1] Title: 큰 놈, 작은 놈, 같은 놈, Time: 92 ms, Memory: 26,752 KB -BaekjoonHub

---
+++ SWEA/D1/2070. 큰 놈， 작은 놈， 같은 놈/큰 놈， 작은 놈， 같은 놈.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+ 
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for(int i=0; i<T; i++) {
+            int n1 = sc.nextInt();
+            int n2 = sc.nextInt();
+            System.out.println("#"+(i+1)+" "+(n1==n2?"=":n1<n2?"<":">"));
+        }
+    }
+}
