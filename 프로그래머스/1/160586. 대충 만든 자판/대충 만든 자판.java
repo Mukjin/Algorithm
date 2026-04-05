4b3b4e0736b67c91d39129c293fc05f9962dd108
Mukjin <127205198+Mukjin@users.noreply.github.com>
2026-04-05 22:45:38 +0900

    [level 1] Title: 대충 만든 자판, Time: 3.20 ms, Memory: 98.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+
+class Solution {
+    public int[] solution(String[] keymap, String[] targets) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        
+        for (String key : keymap) {
+            for (int i = 0; i < key.length(); i++) {
+                char c = key.charAt(i);
+                map.put(c, Math.min(map.getOrDefault(c, Integer.MAX_VALUE), i + 1));
+            }
+        }
+        
+        int[] answer = new int[targets.length];
+        for (int i = 0; i < targets.length; i++) {
+            int sum = 0;
+            for (char c : targets[i].toCharArray()) {
+                if (!map.containsKey(c)) {
+                    sum = -1;
+                    break;
+                }
+                sum += map.get(c);
+            }
+            answer[i] = sum;
+        }
+        return answer;
+    }
+}
